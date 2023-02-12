8ca0fed4975cf6d1b7425f20f4cd8afd6a395586
yamaguchi0912 <81159430+yamaguchi0912@users.noreply.github.com>
2023-02-13 01:53:19 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,27 @@
-self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Install');
+// キャッシュファイルの指定
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+    '/poster-keisuke.github.io/',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activate');
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
 });
-
-self.addEventListener('fetch', function(event) {});
