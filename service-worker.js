de683989c026e6f8a450baff1752e9c3b3b1a5de
yamaguchi0912 <81159430+yamaguchi0912@users.noreply.github.com>
2023-02-13 16:18:26 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,11 @@
-// キャッシュファイルの指定
-var CACHE_NAME = 'pwa-sample-caches';
-var urlsToCache = [
-    '/poster-keisuke.github.io/',
-];
-
-// インストール処理
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
-});
+// service-worker.js
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+  });
+  
+  self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+  });
+  
+  // 現状では、この処理を書かないとService Workerが有効と判定されないようです
+  self.addEventListener('fetch', function(event) {});
